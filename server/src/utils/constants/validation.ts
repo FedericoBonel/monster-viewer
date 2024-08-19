15fafd9d2f92ddd735d1b7d4b84ee475931739a7
Federico Jorge Bonel Tozzi <Bonelfederico@gmail.com>
2024-08-20 00:31:36 +0900

    Fixed another small issue with date validation in server

--- server/src/utils/constants/validation.ts
+++ server/src/utils/constants/validation.ts
@@ -4,7 +4,7 @@ const validation: ValidationValues = {
     lessons: {
         dateFrom: {
             daysBefore: 60,
-            daysAfter: 13,
+            daysAfter: 14,
         },
     },
     pagination: {
