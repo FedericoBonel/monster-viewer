ae7b7a3f55b9c11fe9047685cbbe4fad152286cf
Federico Jorge Bonel Tozzi <Bonelfederico@gmail.com>
2024-08-21 21:35:53 +0900

    Added logging messages for the log in process

--- cron/src/scrapers/bmonster/login.ts
+++ cron/src/scrapers/bmonster/login.ts
@@ -1,5 +1,6 @@
 import { Browser } from "puppeteer";
 import htmlSelectors from "@/utils/constants/selectors";
+import logger from "@/utils/logger";
 
 /** Logs into the bmonster website in the browser */
 async function login(
@@ -9,6 +10,8 @@ async function login(
 ): Promise<void> {
     const page = await browser.newPage();
     page.setViewport({ width: 1920, height: 1080 });
+
+    logger.info("Logging in");
 
     await page.goto("https://b-monster.hacomono.jp/home");
 
@@ -24,6 +27,8 @@ async function login(
         page.waitForNavigation(),
         page.click(`${htmlSelectors.loginForm} button`),
     ]);
+
+    logger.info("Logged in");
 
     await page.close();
 }
